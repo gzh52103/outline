aeb130b4a454e8cba2627be905064da97d794cb4
aaron <jeroo@qq.com>
2021-06-24 12:08:49 +0800

    day2-4 websocket实现服务器效果

--- w2_websocket/src/server.js
+++ w2_websocket/src/server.js
@@ -1,15 +1,27 @@
 const ws = require('ws')
+const http = require('http');
 const express = require('express')
 
+// 创建express服务器
 const app = express();
 app.use(express.static('../public'))
-app.listen(2103,()=>{
-    console.log('web服务器启动成功')
+// app.listen(2103,()=>{
+//     console.log('web服务器启动成功')
+// })
+
+// 利用http连接express
+let server = http.Server(app);
+
+// 必须使用http模块创建的服务器来监听端口
+server.listen(2103,()=>{
+    console.log('web server and websocket are runing');
 })
 
 // 创建websocket服务器
 let wss = new ws.Server({
-    port: 1001
+    // 利用http连接socket服务器
+    server,
+    // port: 1001
 });
 
 wss.on('connection',(client)=>{
@@ -19,7 +31,12 @@ wss.on('connection',(client)=>{
     client.on('message',(msg)=>{
         console.log('msg=',msg);
 
-        client.send(msg.repeat(2))
+        // client.send(msg.repeat(2))
+
+        // 转发消息给所有客户端
+        wss.clients.forEach(item=>{
+            item.send(msg);
+        })
     })
 })
 
